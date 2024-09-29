e4a7645022f7bdd6e99787e24c6d943dc7f477cd
robinborth <robin.borth@tum.de>
2024-09-29 14:35:58 +0200

    feat: add c++ simple point support

--- lib/sphere_rasterizer/point3d.h
+++ lib/sphere_rasterizer/point3d.h
@@ -1,21 +1,38 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtx/norm.hpp> // For distance functions
 #include <tuple>
 
 class Point3D
 {
 public:
-    float x, y, z;
+    glm::vec3 point;
 
-    Point3D(float x, float y, float z) : x(x), y(y), z(z) {}
+    // Constructor using glm::vec3
+    Point3D(float x, float y, float z) : point(x, y, z) {}
 
+    // Set new coordinates
     void set(float new_x, float new_y, float new_z)
     {
-        x = new_x;
-        y = new_y;
-        z = new_z;
+        point = glm::vec3(new_x, new_y, new_z);
     }
 
+    // Get the coordinates as a tuple
     std::tuple<float, float, float> get() const
     {
-        return std::make_tuple(x, y, z);
+        return std::make_tuple(point.x, point.y, point.z);
+    }
+
+    // Calculate distance to another Point3D
+    float distanceTo(const Point3D &other) const
+    {
+        return glm::distance(point, other.point);
+    }
+
+    // Normalize the point and return a new Point3D
+    Point3D normalize() const
+    {
+        glm::vec3 normalized_vec = glm::normalize(point);
+        return Point3D(normalized_vec.x, normalized_vec.y, normalized_vec.z);
     }
 };
--- lib/sphere_rasterizer/pybind11.cpp
+++ lib/sphere_rasterizer/pybind11.cpp
@@ -9,7 +9,15 @@ PYBIND11_MODULE(sphere_rasterizer, m)
         .def(py::init<float, float, float>())
         .def("set", &Point3D::set)
         .def("get", &Point3D::get)
-        .def_readwrite("x", &Point3D::x)  // Expose x
-        .def_readwrite("y", &Point3D::y)  // Expose y
-        .def_readwrite("z", &Point3D::z); // Expose z
+        .def("distance", &Point3D::distanceTo) // Bind distanceTo function
+        .def("normalize", &Point3D::normalize) // Bind normalize function
+        .def_property("x", [](const Point3D &p)
+                      { return p.point.x; }, [](Point3D &p, float x)
+                      { p.point.x = x; }) // Expose x
+        .def_property("y", [](const Point3D &p)
+                      { return p.point.y; }, [](Point3D &p, float y)
+                      { p.point.y = y; }) // Expose y
+        .def_property("z", [](const Point3D &p)
+                      { return p.point.z; }, [](Point3D &p, float z)
+                      { p.point.z = z; }); // Expose z
 }
